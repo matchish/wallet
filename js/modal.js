0829c6fc8d95c2129339453e11f8591a347ae1dc
Aleksandrov Sergey <39244997+Serg-develop@users.noreply.github.com>
2019-09-20 21:08:03 +0300

    commit

--- js/modal.js
+++ js/modal.js
@@ -1,37 +1,145 @@
-document.querySelector(".wallet-instructions-send-android").addEventListener("click", () => {
-  document.querySelector(".wallet-instructions-send__modal-android").style.display = "block";
+window.addEventListener("resize", () => {
+  document
+    .querySelector(".wallet-instructions-send-android")
+    .addEventListener("click", () => {
+      if (window.innerWidth < 1300) {
+        document.querySelector(
+          ".wallet-instructions-send__modal-android"
+        ).style.display = "block";
+      }
+    });
+
+  document
+    .querySelector(".wallet-instructions-close-android")
+    .addEventListener("click", () => {
+      document.querySelector(
+        ".wallet-instructions-send__modal-android"
+      ).style.display = "none";
+    });
+
+  document
+    .querySelector(".wallet-instructions-transactions-android")
+    .addEventListener("click", () => {
+      if (window.innerWidth < 1300) {
+        document.querySelector(
+          ".wallet-instructions-transactions__modal-android"
+        ).style.display = "block";
+      }
+    });
+
+  document
+    .querySelector(".wallet-instructions-transactions__close-android")
+    .addEventListener("click", () => {
+      document.querySelector(
+        ".wallet-instructions-transactions__modal-android"
+      ).style.display = "none";
+    });
+
+  document
+    .querySelector(".wallet-instructions-privacy-android")
+    .addEventListener("click", () => {
+      if (window.innerWidth < 1300) {
+        document.querySelector(
+          ".wallet-instructions-privacy__modal-android"
+        ).style = "display:block;text-align:left;";
+      }
+    });
+
+  document
+    .querySelector(".wallet-instructions-privacy__close-android")
+    .addEventListener("click", () => {
+      document.querySelector(
+        ".wallet-instructions-privacy__modal-android"
+      ).style.display = "none";
+    });
+
+  document
+    .querySelector(".wallet-instructions-receive-android")
+    .addEventListener("click", () => {
+      if (window.innerWidth < 1300) {
+        document.querySelector(
+          ".wallet-instructions-receive__modal-android"
+        ).style = "display:block;text-align:left;";
+      }
+    });
+
+  document
+    .querySelector(".wallet-instructions-receive__close-android")
+    .addEventListener("click", () => {
+      document.querySelector(
+        ".wallet-instructions-receive__modal-android"
+      ).style.display = "none";
+    });
 });
 
+document
+  .querySelector(".wallet-instructions-send-android")
+  .addEventListener("click", () => {
+    if (window.innerWidth < 1300) {
+      document.querySelector(
+        ".wallet-instructions-send__modal-android"
+      ).style.display = "block";
+    }
+  });
 
-document.querySelector(".wallet-instructions-close-android").addEventListener("click",()=>{
-  document.querySelector(".wallet-instructions-send__modal-android").style.display = "none";
-})
+document
+  .querySelector(".wallet-instructions-close-android")
+  .addEventListener("click", () => {
+    document.querySelector(
+      ".wallet-instructions-send__modal-android"
+    ).style.display = "none";
+  });
 
+document
+  .querySelector(".wallet-instructions-transactions-android")
+  .addEventListener("click", () => {
+    if (window.innerWidth < 1300) {
+      document.querySelector(
+        ".wallet-instructions-transactions__modal-android"
+      ).style.display = "block";
+    }
+  });
 
-document.querySelector(".wallet-instructions-transactions-android").addEventListener("click", () => {
-  document.querySelector(".wallet-instructions-transactions__modal-android").style.display = "block";
-});
+document
+  .querySelector(".wallet-instructions-transactions__close-android")
+  .addEventListener("click", () => {
+    document.querySelector(
+      ".wallet-instructions-transactions__modal-android"
+    ).style.display = "none";
+  });
 
+document
+  .querySelector(".wallet-instructions-privacy-android")
+  .addEventListener("click", () => {
+    if (window.innerWidth < 1300) {
+      document.querySelector(
+        ".wallet-instructions-privacy__modal-android"
+      ).style = "display:block;text-align:left;";
+    }
+  });
 
-document.querySelector(".wallet-instructions-transactions__close-android").addEventListener("click",()=>{
-  document.querySelector(".wallet-instructions-transactions__modal-android").style.display = "none";
-})
+document
+  .querySelector(".wallet-instructions-privacy__close-android")
+  .addEventListener("click", () => {
+    document.querySelector(
+      ".wallet-instructions-privacy__modal-android"
+    ).style.display = "none";
+  });
 
+document
+  .querySelector(".wallet-instructions-receive-android")
+  .addEventListener("click", () => {
+    if (window.innerWidth < 1300) {
+      document.querySelector(
+        ".wallet-instructions-receive__modal-android"
+      ).style = "display:block;text-align:left;";
+    }
+  });
 
-document.querySelector(".wallet-instructions-privacy-android").addEventListener("click", () => {
-  document.querySelector(".wallet-instructions-privacy__modal-android").style= "display:block;text-align:left;";
-});
-
-
-document.querySelector(".wallet-instructions-privacy__close-android").addEventListener("click",()=>{
-  document.querySelector(".wallet-instructions-privacy__modal-android").style.display = "none";
-})
-
-document.querySelector(".wallet-instructions-receive-android").addEventListener("click", () => {
-  document.querySelector(".wallet-instructions-receive__modal-android").style= "display:block;text-align:left;";
-});
-
-
-document.querySelector(".wallet-instructions-receive__close-android").addEventListener("click",()=>{
-  document.querySelector(".wallet-instructions-receive__modal-android").style.display = "none";
-})
+document
+  .querySelector(".wallet-instructions-receive__close-android")
+  .addEventListener("click", () => {
+    document.querySelector(
+      ".wallet-instructions-receive__modal-android"
+    ).style.display = "none";
+  });
