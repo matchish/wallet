93f5fdce130367d0fca3833d683c21c0c0c32a21
Aleksandrov Sergey <39244997+Serg-develop@users.noreply.github.com>
2019-09-20 12:04:25 +0300

    commit

--- js/modal.js
+++ js/modal.js
@@ -6,3 +6,32 @@ document.querySelector(".wallet-instructions-send-android").addEventListener("cl
 document.querySelector(".wallet-instructions-close-android").addEventListener("click",()=>{
   document.querySelector(".wallet-instructions-send__modal-android").style.display = "none";
 })
+
+
+document.querySelector(".wallet-instructions-transactions-android").addEventListener("click", () => {
+  document.querySelector(".wallet-instructions-transactions__modal-android").style.display = "block";
+});
+
+
+document.querySelector(".wallet-instructions-transactions__close-android").addEventListener("click",()=>{
+  document.querySelector(".wallet-instructions-transactions__modal-android").style.display = "none";
+})
+
+
+document.querySelector(".wallet-instructions-privacy-android").addEventListener("click", () => {
+  document.querySelector(".wallet-instructions-privacy__modal-android").style= "display:block;text-align:left;";
+});
+
+
+document.querySelector(".wallet-instructions-privacy__close-android").addEventListener("click",()=>{
+  document.querySelector(".wallet-instructions-privacy__modal-android").style.display = "none";
+})
+
+document.querySelector(".wallet-instructions-receive-android").addEventListener("click", () => {
+  document.querySelector(".wallet-instructions-receive__modal-android").style= "display:block;text-align:left;";
+});
+
+
+document.querySelector(".wallet-instructions-receive__close-android").addEventListener("click",()=>{
+  document.querySelector(".wallet-instructions-receive__modal-android").style.display = "none";
+})
